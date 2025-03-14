edf583a7caf45a7deb8f89ad65bad4a4ce3ddf43
kru42 <kru42@kru42.kru>
2025-03-14 23:54:33 +0100

    fix

--- src/taifuse.c
+++ src/taifuse.c
@@ -3,10 +3,11 @@
 
 void kuConsolePrintf(const char* format, ...)
 {
+    char    buffer[CONSOLE_MAX_LINE_LENGTH];
     va_list args;
     va_start(args, format);
-
-    console_log(format, args);
-
+    vsnprintf(buffer, sizeof(buffer), format, args);
     va_end(args);
+
+    console_log("%s", buffer);
 }
